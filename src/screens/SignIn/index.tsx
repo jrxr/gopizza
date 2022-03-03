0f5c6105e2eb4b60e19726e61d7e595de7b52afc
Agacy junior <juniormartinssom@gmail.com>
2022-03-02 21:29:30 -0300

    Finish interface SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
-import { Button } from '@components/Button';
-import { Input } from '@components/Input';
+import brandImg from "../../assets/brand.png";
 
-import { Container } from './styles';
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPassword,
+  ForgotPasswordLabel,
+} from "./styles";
 
 export function SignIn() {
   return (
     <Container>
-      <Input 
-        placeholder="E-mail" 
-        type="secondary" 
-        autoCorrect={false} 
-        autoCapitalize="none"
-      />
-
-      <Input 
-        placeholder="Senha" 
-        type="secondary" 
-        secureTextEntry
-      />
-
-      <Button
-        title="Entrar"
-        type="secondary"
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" type="secondary" secureTextEntry />Î
+          <ForgotPassword>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPassword>
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
