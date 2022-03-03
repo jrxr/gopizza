03e1524b35893cb3297c199c5e14cea196c648e0
Agacy junior <juniormartinssom@gmail.com>
2022-03-03 16:19:42 -0300

    Button back concluid

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Platform, TouchableOpacity } from 'react-native';
+import { ButtonBack } from "../../components/ButtonBack";
+import React from "react";
+import { Platform, TouchableOpacity } from "react-native";
 
-import { Container, Header, Title, DeleteLabel } from './styles';
+import { Container, Header, Title, DeleteLabel } from "./styles";
 
 export function Product() {
   return (
-    <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+    <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Header>
+        <ButtonBack />
         <Title>Cadastrar</Title>
         <TouchableOpacity>
           <DeleteLabel>Deletar</DeleteLabel>
         </TouchableOpacity>
       </Header>
     </Container>
-  )
+  );
 }
